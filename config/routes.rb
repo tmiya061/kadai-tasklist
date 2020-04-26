19e7106eebd88cd7de205f95d60fa09388022406
takuro.miyagawa <taku000@live.jp>
2020-04-26 08:21:09 +0000

    change-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+
   root to: 'tasks#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
